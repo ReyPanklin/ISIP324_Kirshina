eb71d8a2f9f0c00dddbffa4661f5cbb15aca5db4
ReyPanklin <Kirshinamargo@gmail.com>
2026-09-18 01:58:57 +0300

    Была добавлена ф-ия добавления продукта с проверками на ошибки и были исправлены очевидные ошибки кода

--- ISIP324_Kirshina/Program.cs
+++ ISIP324_Kirshina/Program.cs
@@ -10,11 +10,11 @@ namespace ISIP324_Kirshina
     {
         enum Category
         {
-            Drinks,
-            Bakery,
-            FastFood,
-            HomeSupplies,
-            Food
+            Drinks = 1,
+            Bakery = 2,
+            FastFood = 3,
+            HomeSupplies = 4,
+            Food = 5
         }
         public class Product
         {
@@ -25,66 +25,98 @@ namespace ISIP324_Kirshina
             public bool isThere;
             public Category category;
 
-            public Products(string name, double cost, int quantity, Category category)
+            public Product(string name, double cost, int quantity, Category category)
             {
                 this.name = name;
                 this.cost = cost;
                 this.quantity = quantity;
                 this.category = category;
-                this.isThere = isThere;
+                this.isThere = quantity > 0;
             }
-            static void Main(string[] args)
+        }
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+            int nextCode = 1;
+            ShowMenu();
+            void ShowMenu()
             {
-                List <Product> products = new List<Product>();
-                ShowMenu();
-                void ShowMenu()
+                Console.WriteLine("Меню:");
+                Console.WriteLine("1.Добвить товар");
+                Console.WriteLine("2. Удалить товар");
+                Console.WriteLine("3. Заказать поставку товара");
+                Console.WriteLine("4. Продать товар");
+                Console.WriteLine("5. Найти товар (по коду, названию и категории)");
+                Console.WriteLine("0. Выход");
+                string choice = Console.ReadLine();
+                switch (choice)
                 {
-                    Console.WriteLine("Меню:");
-                    Console.WriteLine("1.Добвить товар");
-                    Console.WriteLine("2. Удалить товар");
-                    Console.WriteLine("3. Заказать поставку товара");
-                    Console.WriteLine("4. Продать товар");
-                    Console.WriteLine("5. Найти товар (по коду, названию и категории)");
-                    Console.WriteLine("0. Выход");
-                    string choice = Console.ReadLine();
-                    switch (choice)
+                    case "1":
+                        AddProduct();
+                        break;
+                    //case "2":
+                    // DeleteProduct();
+                    // break;
+                    //case "3":
+                    // OrderDelivery();
+                    // break;
+                    //case "4":
+                    // SellProdcut();
+                    // break;
+                    //case "5":
+                    // SearchProduct();
+                    // break;
+                    //case "0":
+                    // return;
+                    default:
+                        Console.WriteLine("Такого варианта нет");
+                        break;
+                }
+                void AddProduct()
+                {
+                    Console.WriteLine("Введите товары в следующем виде: Название; цена; кол-во");
+                    Console.WriteLine("Категории: 1 - Drinks, 2 - Bakery, 3 - FastFood, 4 - HouseSupplies, 5 - Food");
+                    string input = Console.ReadLine();
+                    string[] parts = input.Split(';');
+                    if (parts.Length != 4)
                     {
-                        case "1":
-                            AddProduct();
-                            break;
-                        //case "2":
-                        // DeleteProduct();
-                        // break;
-                        //case "3":
-                        // OrderDelivery();
-                        // break;
-                        //case "4":
-                        // SellProdcut();
-                        // break;
-                        //case "5":
-                        // SearchProduct();
-                        // break;
-                        //case "0":
-                        // return;
-                        default:
-                            Console.WriteLine("Такого варианта нет");
-                            break;
+                        Console.WriteLine("Неверный формат! Нужно 4 значения через точку с запятой.");
+                        return;
                     }
-                    void AddProduct()
+                    string name = parts[0].Trim();
+                    if (name.Length == 0)
                     {
-                        Console.WriteLine("Введите товары в следующем виде: Название; цена; кол-во");
-                        string input = Console.ReadLine();
-                        string[] parts = input.Split(';');
-                        if (parts.Length == 3)
-                        {
-                            products = parts[0].Trim();
-
-                        }
-                        else
-                        {
-
-                        }
+                        Console.WriteLine("Строка не может быть пустой!");
+                        return;
                     }
+                    double cost = Convert.ToDouble(parts[1].Trim());
+                    if (cost < 0)
+                    {
+                        Console.WriteLine("Цена не может быть отрицательной!");
+                        return;
+                    }
+                    int quantity = Convert.ToInt32(parts[2].Trim());
+                    if (quantity < 0)
+                    {
+                        Console.WriteLine("Кол-во не может быть отрицательным");
+                        return;
+                    }
+                    int categoryNum = Convert.ToInt32(parts[3].Trim());
+
+                    if (categoryNum < 1 || categoryNum > 5)
+                    {
+                        Console.WriteLine("Категория должна быть от 1 до 5!");
+                        return;
+                    }
+                    Category category = (Category)categoryNum;
+                    Product newProduct = new Product(name, cost, quantity, category);
+                    newProduct.code = nextCode;
+                    nextCode++;
+
+                    newProduct.isThere = quantity > 0;
+
+                    products.Add(newProduct);
+                    Console.WriteLine($"Товар {name} добвлен с кодом {newProduct.code} в кол-ве {quantity}, категории {newProduct.category}");
                 }
             }
         }
